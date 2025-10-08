2023addd3a23cd0c97790d9a96890057db7bc4dc
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-10-08 20:49:39 -0300

    feat: cria header para a aplicação.

--- sistema/src/App.tsx
+++ sistema/src/App.tsx
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-import { Outlet } from "react-router-dom";
-
-export default function App () {
-  return (
-    <>
-    <Outlet/>
-    </>
-=======
 export default function App(){
 
   return(
     <main>
       <h1>App</h1>
     </main>
->>>>>>> 88a57fca699c309c968cef8e2e87522cb9de1601
   );
 }
