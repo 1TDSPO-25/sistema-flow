fb4a3468c75c173c1ab93e899612fc6d2905b824
MatheusSousaAlmeida <math.almeida.sousa@gmail.com>
2025-10-08 20:57:03 -0300

    chore: Criação da rota Noticias e criação do path noticias.

--- sistema/src/main.tsx
+++ sistema/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.tsx'
+import Noticias from './routes/Noticias/index.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {path:"/", element:<App/>},
+  {path:"/noticias", element:<Noticias/>}
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router}/>
   </StrictMode>,
 )
