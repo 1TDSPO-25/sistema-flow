3106b3b3e0a4af87eb10db733eb1d180c2444139
grazialencar <graziellealencar3@gmail.com>
2025-10-13 23:14:48 -0300

    Import do global.css na main

--- sistema/src/main.tsx
+++ sistema/src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import App from './App.tsx'
 import Error from './routes/Error/index.tsx';
 import Home from './routes/Home/index.tsx'
@@ -8,6 +7,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import Cadastro from './routes/Cadastro/index.tsx';
 import Login from './routes/Login/index.tsx';
+
+import './global.css'
 
 const router = createBrowserRouter([
     {
@@ -27,24 +28,4 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './App.tsx'
-import Noticias from './routes/Noticias/index.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-
-const router = createBrowserRouter([
-  {path:"/", element:<App/>},
-  {path:"/noticias", element:<Noticias/>}
-]);
-
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-     <RouterProvider router={router}/>
-  </StrictMode>,
->>>>>>> 88a57fca699c309c968cef8e2e87522cb9de1601
 )
