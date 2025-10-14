771a9a1a3bf590434929a5b584ba16fa2f8b0a66
JonasSantoos <jscom3121@gmail.com>
2025-10-13 22:19:10 -0300

    feat: busca dados do tempo

--- sistema/src/components/CardWeather/CardWeather.tsx
+++ sistema/src/components/CardWeather/CardWeather.tsx
@@ -19,4 +19,23 @@ export default function CardWeather(){
     const[error, setError] = useState("");
 
     const apiKey = "c0a23509a1bfccf9a4c75934e9cf09c8";
+
+    async function getWeather(city: string){
+        try{
+            setLoading(true);
+            setError(null);
+            const res = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${c0a23509a1bfccf9a4c75934e9cf09c8}&units=metric&lang=pt_br`
+            );
+            if(!res.ok) throw new Error("Cidade não encontrada");
+            const json = await res.json();
+            setData(json);
+            } catch(err : any){
+                setError(err.message);
+                setData(null);
+            } finally{
+                setLoading(false);
+            }
+            
+        }
 }
