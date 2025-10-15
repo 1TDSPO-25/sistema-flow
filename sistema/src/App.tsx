32709f475a3e48d4e09fc8b9ad0915001762563b
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-10-15 20:48:58 -0300

    feat: Adiciona importação dos components na app.

--- sistema/src/App.tsx
+++ sistema/src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 
 export default function App(){
 
@@ -7,6 +8,7 @@ export default function App(){
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
