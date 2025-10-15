4b58d2a5c4eed67e75dcc62692a26aaf4e591ef6
PedroBiasolli <pedrobiasolli@gmail.com>
2025-10-15 20:35:32 -0300

    Adição das imagens ao carrossel

---
+++ sistema/src/Components/Produtos/carroselProdutos.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+ 
+const Carrossel = () => {
+  const products = [
+    { id: 1, nome: 'Cama de cachorro', imgSrc: '../../img/cama.png' },
+    { id: 2, nome: 'Ração Premium', imgSrc: '../../img/raçao.jpg' },
+    { id: 3, nome: 'Escova de pelos', imgSrc: '../../img/Escova_Pet.jpg' },
+    { id: 4, nome: 'Bolinha de borracha', imgSrc: '../../img/Bolinha_Cachorro.jpg' },
+    { id: 5, nome: 'Coleira para pets', imgSrc: '../../img/Coleira.jpg' },
+    { id: 6, nome: 'Caixa transportadora de gatos', imgSrc: '../../img/Caixa_Transportadora_Gatos.jpg' },
+    { id: 7, nome: 'Arranhador para gatos simples', imgSrc: '../../img/Arranhador_Gatos.jpg' },
+    { id: 8, nome: 'Arranhador para gatos premium', imgSrc: '../../img/Brinquedo_Gatos_premium.jpg' }
+  ];}
