ad8ee588a8ce918fbd63a62c72012ba746f653ef
JonasSantoos <jscom3121@gmail.com>
2025-10-20 22:33:43 -0300

    WIP: Salvando alterações antes de iniciar o git flow

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import Cadastro from './routes/Cadastro/index.tsx';
 import Login from './routes/Login/index.tsx';
-<<<<<<< HEAD
-import App from './App.tsx'
-<<<<<<< HEAD
-=======
->>>>>>> a9e7bf185c216608c45b63ef526ced5d71b30a1e
 import Noticias from './routes/Noticias/index.tsx'
 import './global.css'
 import Home from './routes/Home/index.tsx';
@@ -30,9 +25,6 @@ const router = createBrowserRouter([
         ]
     }
 ]);
-=======
-import './globals.css'
->>>>>>> feature/css-tailwind-responsivo
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
