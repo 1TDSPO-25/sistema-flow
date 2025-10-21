b93d8d4a1ac4d5b455ee8165e274ee4e4bbeca64
Bruno-A-Z <zanateli07@hotmail.com>
2025-10-21 01:25:06 -0300

    feat: adicionar controle de estado para login e senha no LoginForm

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h2>Login</h2>
-      <form>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <input
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Entrar na conta</button>
       </form>
     </div>
   );
