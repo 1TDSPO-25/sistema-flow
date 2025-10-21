0417829f8ef3b387148e2b41f0aa4112f58372f5
Bruno-A-Z <zanateli07@hotmail.com>
2025-10-21 01:52:05 -0300

    feat: alterei o titulo da pagina de login e fiz o import do LoginForm

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,7 +1,10 @@
-export default function Login(){
-    return(
-        <main>
-            <h1>Login</h1>
-        </main>
-    );
+import { LoginForm } from "../../components/Login/LoginForm";
+
+export default function Login() {
+  return (
+    <main>
+      <h1>Página de Login</h1>
+      <LoginForm/>
+    </main>
+  );
 }
