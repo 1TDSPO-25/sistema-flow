815681f1bad1fc89c317b215a67ad69f771db875
EnzoMMaciel10 <enzomontmaciel@gmail.com>
2025-10-22 20:09:37 -0300

    feat: Adicionando e estilizando o campo login.

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,7 +1,12 @@
-export default function Login(){
-    return(
-        <main>
-            <h1>Login</h1>
-        </main>
-    );
+export default function Login() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-300 p-4">
+      <section className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md md:max-w-lg lg:max-w-xl">
+        <h1 className="text-3xl md:text-4xl font-bold text-center text-orange-500 mb-8">
+          Login
+        </h1>
+        
+      </section>
+    </main>
+  );
 }
