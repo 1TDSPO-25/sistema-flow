921254c595c34f7467460998b993c501580a0eb8
gabrielCabralmm <gabriel.cmmariano@gmail.com.br>
2025-10-22 20:52:10 -0300

    feat: tipando os inputs do formulário de cadastro.

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -6,7 +6,7 @@ export default function Cadastro() {
                 <div className="flex flex-col justify-center w-100 gap-5 mx-auto p-2">
                     <input type="text" placeholder="Nome" className="bg-gray-200 py-2 px-3 rounded-full shadow-2xs"/>
                     <input type="email" placeholder="Nome" className="bg-gray-200 py-2 px-3 rounded-full shadow-2xs"/>
-                    
+                    <input type="password" placeholder="Nome" className="bg-gray-200 py-2 px-3 rounded-full shadow-2xs"/>
                 </div>
                 <div className="flex justify-center">
                     <button className="text-lg p-2 bg-orange-500 text-white font-semibold rounded-full shadow-md hover:bg-orange-400 transition-all">Cadastrar-se</button>
