8705876a1c792020c6190a60ea71c5af7eba11b1
gabrielCabralmm <gabriel.cmmariano@gmail.com.br>
2025-10-22 20:51:57 -0300

    feat: Estilizando os campos do formulário.

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -5,6 +5,7 @@ export default function Cadastro() {
                 <h1 className="text-2xl">Cadastro</h1>
                 <div className="flex flex-col justify-center w-100 gap-5 mx-auto p-2">
                     <input type="text" placeholder="Nome" className="bg-gray-200 py-2 px-3 rounded-full shadow-2xs"/>
+                    <input type="email" placeholder="Nome" className="bg-gray-200 py-2 px-3 rounded-full shadow-2xs"/>
                     
                 </div>
                 <div className="flex justify-center">
