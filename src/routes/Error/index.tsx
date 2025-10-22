885edfedb35dcef02dd8bab1bfb427456aac7e12
Ferreira <pedroferreira.gomes03@gmail.com>
2025-10-22 20:01:24 -0300

    feat: iniciando construcao de pag de error

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -2,6 +2,10 @@ import cachorrinhoError from '../../assets/cachorro.png';
 
 export default function Error(){
     return(
-        
+        <main className='bg-[#c2c1c1] w-full h-[100vh] flex items-center justify-center'>
+            <div className='w-[80%] flex items-center justify-center'>
+                
+            </div>
+        </main>
     );
 }
