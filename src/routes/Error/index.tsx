64800882d3032fa59bda5ba8c123cfa184e8018f
gabrielCabralmm <gabriel.cmmariano@gmail.com.br>
2025-10-22 19:51:33 -0300

    Estilizando a página de erro.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,7 +1,9 @@
 export default function Error(){
     return(
         <main>
-            <h1>Error 404 - Página não encontrada</h1>
+            <div className="bg-linear-to-r from-gray-800 via-gray-700 to-gray-800 text-white shadow-2xl rounded-3xl p-5 w-200 mx-auto text-center mt-50">
+                <h1 className="border-b-1 border-orange-400 text-3xl p-3">Error 404 - Página não encontrada</h1>
+            </div>
         </main>
     );
 }
