d7300ec8631c3efd6c06adff91ea2a5919f591b3
phestalise <phestalise@gmail.com>
2025-10-22 20:45:13 -0300

    feat: adicionar esqueleto do componente ProdutoDetalhe - commit

---
+++ src/routes/ProdutoDetalhe/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProdutoDetalhe() {
+  return (
+    <section className="max-w-6xl mx-auto px-6 py-10">
+      <p>Carregando detalhe do produto...</p>
+    </section>
+  );
+}
