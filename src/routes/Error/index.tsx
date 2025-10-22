0d649b4ed4ef2c1649b1155166049a545126ab31
Ferreira <pedroferreira.gomes03@gmail.com>
2025-10-22 20:00:39 -0300

    feat: importando imagem da pagina de error

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,7 +1,7 @@
+import cachorrinhoError from '../../assets/cachorro.png';
+
 export default function Error(){
     return(
-        <main>
-            <h1>Error 404 - Página não encontrada</h1>
-        </main>
+        
     );
 }
