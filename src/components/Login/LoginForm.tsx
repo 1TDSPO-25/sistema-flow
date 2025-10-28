bb2681b108094c86e9bde0831c113a0bc171ff9e
Bruno-A-Z <zanateli07@hotmail.com>
2025-10-27 22:02:38 -0300

    feat: adicionado o teste/validacao das credenciais

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -12,6 +12,12 @@ export function LoginForm() {
     const validEmail = "1tdspo@fiap.com.br"; //Email com o nome da sala
     const validPassword = "281025"; //Dia da entrega da CP  
     
+    if (email === validEmail && password === validPassword) {
+      console.log("Login bem-sucedido:", { email, password });
+    } else {
+      console.warn("Falha no login:", { email, password });
+    }
+
    };
     
     
