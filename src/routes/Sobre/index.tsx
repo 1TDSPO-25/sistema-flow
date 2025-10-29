e09b4e0e676102d7bfc2f17ec087fe3ff78fbe7c
diandrade <contato.andradediego@gmail.com>
2025-10-29 20:40:25 -0300

    chore: Componentização da section nossa história.

--- src/routes/Sobre/index.tsx
+++ src/routes/Sobre/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { FaHeart, FaShieldAlt, FaLightbulb } from 'react-icons/fa';
-import ImgEquipe from '../../assets/foto-equipe.png';
 import { HeroSobre } from '../../components/HeroSobre/HeroSobre';
+import { NossaHistoria } from '../../components/NossaHistoria/NossaHistoria';
 
 export default function Sobre(){
     const nossosValores = [
@@ -25,28 +24,7 @@ export default function Sobre(){
   return (
     <main className="bg-slate-50 text-slate-800">
         <HeroSobre />
-      <section className="py-20">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-            <div>
-              <img 
-                src={ImgEquipe} 
-                alt="Nossa clínica ou equipe" 
-                className="rounded-lg shadow-xl w-full h-auto object-cover"
-              />
-            </div>
-            <div>
-              <h2 className="text-3xl font-bold mb-6">Como tudo começou</h2>
-              <p className="text-slate-600 mb-4 text-lg">
-                Nossa jornada começou em 2015, não como um negócio, mas como uma missão. Vimos a necessidade de um lugar que não apenas cuidasse da saúde, mas que também entendesse a alma de cada animal.
-              </p>
-              <p className="text-slate-600 text-lg">
-                Começamos com um pequeno consultório e um grande sonho. Hoje, somos uma equipe completa de veterinários, tosadores e cuidadores dedicados, mas aquele carinho inicial ainda é o coração de tudo que fazemos.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+        <NossaHistoria />
       <section className="bg-slate-100 py-20">
         <div className="container mx-auto px-4 text-center">
           <h2 className="text-3xl font-bold mb-4">Nossos Pilares</h2>
