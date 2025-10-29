594a867d4e496a4bff5f5f275bfa87b606ad8589
diandrade <contato.andradediego@gmail.com>
2025-10-29 20:01:14 -0300

    feat: Adição de chamada principal da página.

--- src/routes/Sobre/index.tsx
+++ src/routes/Sobre/index.tsx
@@ -22,7 +22,16 @@ export default function Sobre(){
 
   return (
     <main className="bg-slate-50 text-slate-800">
-      {}
+      {<section className="bg-slate-100 py-24">
+        <div className="container mx-auto px-4 text-center">
+          <h1 className="text-4xl md:text-5xl font-extrabold mb-4">
+            Construído com Paixão por Pets
+          </h1>
+          <p className="text-lg md:text-xl max-w-3xl mx-auto text-slate-600">
+            Conheça a história e os valores que nos tornam a segunda casa do seu melhor amigo.
+          </p>
+        </div>
+      </section>}
     </main>
   );
 }
