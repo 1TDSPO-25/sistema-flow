a89174e4210ff8a17fcd54592259d69be54bad28
AlefRodrigues94 <rm563272@fiap.com.br>
2025-10-29 20:31:06 -0300

    feat: criar componente base da página de serviços

---
+++ src/routes/Servicos/index.tsx
@@ -0,0 +1,14 @@
+export default function Servicos() {
+  return (
+    <main className="bg-gray-100 text-gray-800 min-h-screen py-20">
+      <section className="container mx-auto px-6 text-center">
+        <h1 className="text-4xl font-extrabold text-orange-500 mb-6">
+          Nossos Serviços
+        </h1>
+        <p className="max-w-2xl mx-auto text-lg mb-12 text-gray-600">
+          Cuidamos do seu pet com carinho, qualidade e segurança. Conheça nossos principais serviços!
+        </p>
+      </section>
+    </main>
+  );
+}
